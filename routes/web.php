b9268d58da2d04081110c6a2b9f77eaad8b4ced1
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-27 05:16:20 +0700

    resources Product

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['middleware' => 'auth'],function(){
     Route::resource('users','UserController');
 });
+Route::resource('products','ProductController');
