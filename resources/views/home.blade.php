fa039c83dfda5c07ef05c4d6cfecf8be8f46e76d
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-27 00:43:12 +0700

    example admin dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,26 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action active">
+                    Dashboard
+                </a>
+            </div>
+        </div>
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header text-center">Dashboard</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Total User</h5>
+                            <div class="display-2">
+                                {{\App\User::get()->count()}}
+                            </div>
+                            <a href="#" class="card-link">Card link</a>
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
