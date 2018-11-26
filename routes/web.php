555b254d2daabd28706c7c5bb9f3d6a0966a83bf
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-27 05:19:00 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'],function(){
     Route::resource('users','UserController');
