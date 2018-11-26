e0aef8c4690584b3a4cd759310fdd3dc8c959ec3
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-27 06:40:50 +0700

    Product Testing

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function produksi_can_create_product()
+    {
+        $produksi = $this->createRole(3);
+
+        $this->actingAs($produksi);
+        $this->get(route('products.index'))->assertOk();
+    }
+
+    /** @test */
+    public function super_admin_can_create_product()
+    {
+        $superAdmin = $this->createRole(1);
+
+        $this->actingAs($superAdmin);
+        $this->get(route('products.index'))->assertOk();
+    }
+
+    /** @test */
+    public function sales_cant_create_product()
+    {
+        $this->withExceptionHandling();
+        $superAdmin = $this->createRole(2);
+        $this->actingAs($superAdmin)
+            ->get('/products')->assertStatus(403);
+    }
+
+    public function createRole($role_id)
+    {
+        return factory('App\User')->create(['role_id' => $role_id]);
+    }
+
+}
