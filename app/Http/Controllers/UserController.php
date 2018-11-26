3d7050cb69a8f3ef88f1aa4000b006a3b1994d0a
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-26 15:29:59 +0700

    create a test case for creating user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function store()
+    {
+        $user = User::create([
+            'name' => request('name'),
+            'role_id' => 3,
+            'email' => request('email'),
+            'password' => \Hash::make(request('password')),
+
+        ]);
+        $this->authorize('create',$user);
+        return request()->wantsJson()
+        ?
+        response()->json($user,201): null;
+    }
+}
--- tests/Feature/UserManagementTest.php
+++ tests/Feature/UserManagementTest.php
@@ -9,10 +9,33 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class UserManagementTest extends TestCase
 {
 
+    use RefreshDatabase;
+
     /** @test */
     public function super_admin_can_create_new_user()
     {
-        $this->assertTrue(true);
+        $user = factory('App\User');
+        /**
+         * Only Super Admin/ Role_id 1 can create user
+         * Every Role that doesnt have role_id 1 will be throw error
+         * Choose the 3 variable below to test using role_id 1/2/or 3
+         * change the $this->actingAs() to your choosen variable
+         * Expectation = using the first/$superAdmin will OK the test
+         * but, using 2nd and 3rd ($sales or $produksi) will throw an error
+         * 'This action is unauthorize'
+         */
+        // $superAdmin = $user->make(['role_id' => 1]);
+        $sales = $user->make(['role_id' => 2]);
+        // $produksi = $user->make(['role_id' => 3]);
+
+        $data = [
+            'name' => 'bebas',
+            'email' => 'tes@gmail.com',
+            'password' => 'password',
+            'role_id' => 3,
+        ];
+        $this->actingAs($sales);
+        $this->postJson(route('users.store'),$data)->assertStatus(201);
     }
 
 }
