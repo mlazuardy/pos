5038d8a2298c08886306c73428693a654ce27767
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-26 14:09:30 +0700

    relationship between User and Role

--- app/Role.php
+++ app/Role.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     protected $guarded = [];
+
+    /**
+     * Role has one User
+     */
+    public function user()
+    {
+        return $this->hasOne('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -27,4 +27,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * User belongsTo Role
+     */
+    public function role()
+    {
+        return $this->belongsTo('App\Role');
+    }
 }
