0e59db8e856351b78373244e8faab9460bb5baba
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-27 04:21:57 +0700

    add link

--- resources/views/users/partials/_layout.blade.php
+++ resources/views/users/partials/_layout.blade.php
@@ -4,7 +4,7 @@
     <div class="row justify-content-center">
         <div class="col-md-4">
             <div class="list-group">
-                <a href="#" class="list-group-item list-group-item-action">
+                <a href="/home" class="list-group-item list-group-item-action">
                     Dashboard
                 </a>
                 <a href="{{route('users.index')}}" class="list-group-item list-group-item-action">
