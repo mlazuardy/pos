1b110a2a7395ad7e3b6c52033aa30d463b6b0261
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-27 05:29:22 +0700

    add product policy

---
+++ app/Policies/ProductPolicy.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Product;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ProductPolicy
+{
+    use HandlesAuthorization;
+
+    public function before(User $user,$ability)
+    {
+        if($user->superAdmin()){
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can view the product.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Product  $product
+     * @return mixed
+     */
+    public function view(User $user, Product $product)
+    {
+        return $user->role_id === 3;
+    }
+
+    /**
+     * Determine whether the user can create products.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user, Product $product)
+    {
+        return $user->role_id === 3;
+    }
+
+
+    /**
+     * Determine whether the user can delete the product.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Product  $product
+     * @return mixed
+     */
+    public function delete(User $user, Product $product)
+    {
+        return $user->role_id === 3;
+    }
+}
