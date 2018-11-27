146b60d93a96ff8ffab1c9651f948ecb152119c4
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-27 07:37:34 +0700

    add card for product

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,8 +11,16 @@
                     <img class="card-img-top" src="{{asset('storage/'.$product->image)}}" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title">{{$product->name}}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-text">
+                            Rp. {{number_format($product->price,0)}}
+                        </p>
+                        <p class="card-text">
+                            Stok: {{$product->stock}}
+                        </p>
+                        <p class="card-text">
+                            Berat: {{$product->berat}}
+                        </p>
+                        <a href="{{route('products.edit',$product->id)}}" class="btn btn-primary">Edit</a>
                     </div>
                 </div>
             </div>
