a857902d30d0c1eb3a3cb12986a59d3e17aea466
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-28 01:10:17 +0700

    custom validation error

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -1,10 +1,10 @@
 <?php
 
 if(!function_exists('hasError')){
-    function hasError($errors,$field,$class= "invalid-feedback"){
+    function hasError($errors,$field){
 
         if ($errors->has($field)) {
-            $say = sprintf('<span %s><strong>%s</strong></span>', $class != null ? "class=$class" : '', $errors->first($field));
+            $say = sprintf('<span class=text-danger><strong>%s</strong></span>',$errors->first($field));
             return $say;
         }
     }
