b2dfb8aa89d25436542f4af00fde1511a1501f1d
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-28 00:55:56 +0700

    add helper for validation

---
+++ app/Http/Helpers/Validation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Helpers;
+
+if(!function_exists('hasError')){
+    function hasError($errors,$field,$class= "invalid-feedback"){
+
+        if ($errors->has($field)) {
+            $say = sprintf('<span %s><strong>%s</strong></span>', $class != null ? "class=$class" : '', $errors->first($field));
+            return $say;
+        }
+    }
+}
