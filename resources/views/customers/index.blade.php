da335bffdd4bdd283dae80437b3efd8ff643a1da
Michael Lazuardy <shawnmilke55@gmail.com>
2018-11-27 21:52:57 +0700

    add button

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,5 +1,7 @@
 @extends('users.partials._layout')
 @section('dashboard-title','Customers')
 @section('dashboard-body')
-    
+    <div class="text-right mb-3">
+        <a href="{{route('customers.create')}}" class="btn btn-primary">Add Customer</a>
+    </div>
 @endsection
