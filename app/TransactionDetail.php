317489133b934cd6a20eb19321444c73e33c0be6
Michael Lazuardy <shawnmilke55@gmail.com>
2018-12-03 12:13:20 +0700

    add

--- app/TransactionDetail.php
+++ app/TransactionDetail.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransactionDetail extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function transaction()
+    {
+        return $this->belongsTo('App\Transaction');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo('App\Product');
+    }
 }
